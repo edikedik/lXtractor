1dff0ead1d0106d23e4609f0237b66588d2a3b20
edikedik <edikedikedikedik@gmail.com>
2022-10-28 16:10:29 +0200

    add `ParallelCalculator`

--- lXtractor/variables/calculator.py
+++ lXtractor/variables/calculator.py
@@ -2,25 +2,31 @@ from __future__ import annotations
 
 import typing as t
 from collections import abc
-from itertools import starmap
+from concurrent.futures import ProcessPoolExecutor
+from itertools import starmap, repeat
 
 from more_itertools import zip_equal
 
 from lXtractor.core.exceptions import FailedCalculation
 from lXtractor.variables.base import AbstractCalculator, OT, RT, VT, MappingT
 
-
 ERT: t.TypeAlias = tuple[bool, t.Union[RT, str]]
 
 
 def _try_calculate(
         o: OT, v: VT, m: MappingT | None,
-        valid_exceptions: abc.Sequence[Exception] = (FailedCalculation, ),
+        valid_exceptions: abc.Sequence[Exception]
 ) -> ERT:
     try:
         return True, v.calculate(o, m)
     except valid_exceptions as e:
         return False, str(e)
+
+
+def _calc_on_object(
+        o: OT, vs: abc.Iterable[VT], m: MappingT | None,
+        valid_exceptions: abc.Sequence[Exception]):
+    return [_try_calculate(o, v, m, valid_exceptions) for v in vs]
 
 
 class SimpleCalculator(AbstractCalculator):
@@ -33,18 +39,64 @@ class SimpleCalculator(AbstractCalculator):
     The calculation has gracefully failed iff class:`lXtractor.core.exceptions.FailedCalculation` is raised.
     """
 
-    __slots__ = ()
+    __slots__ = ('valid_exceptions',)
+
+    def __init__(self, valid_exceptions: abc.Sequence[Exception] = (FailedCalculation,)):
+        self.valid_exceptions = valid_exceptions
 
     def __call__(self, o: OT, v: VT, m: MappingT | None) -> ERT:
-        return _try_calculate(o, v, m)
+        return _try_calculate(o, v, m, self.valid_exceptions)
 
     def map(self, o: OT, v: abc.Iterable[VT], m: MappingT | None) -> abc.Iterator[ERT]:
-        return map(lambda _v: _try_calculate(o, _v, m), v)
+        return map(lambda _v: _try_calculate(o, _v, m, self.valid_exceptions), v)
 
     def vmap(
             self, o: abc.Iterable[OT], v: VT, m: abc.Iterable[MappingT | None]
     ) -> abc.Iterator[ERT]:
-        return starmap(lambda _o, _m: _try_calculate(_o, v, _m), zip_equal(o, m))
+        return starmap(
+            lambda _o, _m: _try_calculate(_o, v, _m, self.valid_exceptions),
+            zip_equal(o, m))
+
+
+class ParallelCalculator(AbstractCalculator):
+    __slots__ = ('num_proc', 'verbose', 'valid_exceptions')
+
+    def __init__(
+            self, num_proc: int | None, verbose: bool = True,
+            valid_exceptions: abc.Sequence[Exception] = (FailedCalculation,)
+    ):
+        self.num_proc = num_proc
+        self.verbose = verbose
+        self.valid_exceptions = valid_exceptions
+
+    def __call__(
+            self, o: abc.Iterable[OT], v: abc.Iterable[abc.Iterable[VT]],
+            m: abc.Iterable[MappingT | None] | None
+    ) -> abc.Iterator[list[ERT]]:
+        if m is None:
+            m = repeat(m)
+        with ProcessPoolExecutor(self.num_proc) as executor:
+            results = executor.map(
+                _calc_on_object, o, v, m, repeat(self.valid_exceptions))
+            yield from results
+
+    def map(
+            self, o: OT, v: abc.Iterable[VT], m: MappingT | None
+    ) -> abc.Iterator[RT]:
+        with ProcessPoolExecutor(self.num_proc) as executor:
+            results = executor.map(
+                _try_calculate, repeat(o), v, repeat(m), repeat(self.valid_exceptions))
+            yield from results
+
+    def vmap(
+            self, o: abc.Iterable[OT], v: VT, m: abc.Iterable[MappingT | None]
+    ) -> abc.Iterator[RT]:
+        if m is None:
+            m = repeat(m)
+        with ProcessPoolExecutor(self.num_proc) as executor:
+            results = executor.map(
+                _try_calculate, o, repeat(v), m, repeat(self.valid_exceptions))
+            yield from results
 
 
 if __name__ == '__main__':
