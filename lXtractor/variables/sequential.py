3acda21cf9c1db58fe066a154490663f11b563d3
Ivan Reveguk <edikedikedikedik@gmail.com>
2022-12-02 19:08:45 +0100

    Adding SliceMapReduce-type of variables

--- lXtractor/variables/sequential.py
+++ lXtractor/variables/sequential.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
 import typing as t
+from abc import abstractmethod
 from collections import abc
+
+from more_itertools import islice_extended
 
 from lXtractor.core.config import SeqNames
 from lXtractor.core.exceptions import FailedCalculation
@@ -9,6 +12,7 @@ from lXtractor.variables.base import SequenceVariable, MappingT, ProtFP
 
 T = t.TypeVar('T')
 V = t.TypeVar('V')
+K = t.TypeVar('K')
 
 _ProtFP = ProtFP()
 
@@ -23,7 +27,6 @@ def try_map(p: T, m: abc.Mapping[T, V] | None):
 
 
 class SeqEl(SequenceVariable):
-
     __slots__ = ('p', 'seq_name')
 
     def __init__(self, p: int, seq_name: str = SeqNames.seq1):
@@ -43,7 +46,6 @@ class SeqEl(SequenceVariable):
 
 
 class PFP(SequenceVariable):
-
     __slots__ = ('p', 'i')
 
     # pfp = ProtFP()
@@ -65,6 +67,67 @@ class PFP(SequenceVariable):
             return _ProtFP[(seq[p - 1], self.i)]
         except (KeyError, IndexError):
             raise FailedCalculation(f'Failed to map {p - 1} with ProtFP')
+
+
+class SliceMapReduce(SequenceVariable, t.Generic[T, V, K]):
+    __slots__ = ('start', 'stop', 'step', 'seq_name')
+
+    def __init__(
+            self, start: int | None = None,
+            stop: int | None = None,
+            step: int | None = None,
+            seq_name: str = SeqNames.seq1
+    ):
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.seq_name = seq_name
+
+    @staticmethod
+    @abstractmethod
+    def reduce(seq: abc.Iterable[T]) -> V:
+        raise NotImplementedError
+
+    @staticmethod
+    def map(seq: abc.Iterator[K]) -> abc.Iterable[T]:
+        return seq
+
+    def calculate(self, seq: abc.Iterable[K], mapping: t.Optional[MappingT] = None) -> V:
+        start, stop, step = map(
+            lambda x: None if x is None else try_map(x, mapping),
+            [self.start, self.stop, self.step]
+        )
+
+        if start is not None:
+            start -= 1
+
+        return self.reduce(self.map(islice_extended(seq, start, stop, step)))
+
+
+def make_rmr(
+        reduce_fn: abc.Callable[[abc.Iterable[T]], V],
+        rtype: t.Type,
+        reduce_fn_name: str | None = None,
+        map_fn_name: str | None = None,
+        map_fn: abc.Callable[[abc.Iterator[K]], abc.Iterable[T]] | None = None
+) -> t.Type[SliceMapReduce]:
+    d = {
+        'reduce': staticmethod(reduce_fn),
+        'rtype': property(lambda _: rtype)}
+
+    if map_fn is None:
+        map_name = ''
+    else:
+        map_name = map_fn_name or map_fn.__name__
+        d['map'] = staticmethod(map_fn)
+
+    reduce_name = reduce_fn_name or reduce_fn.__name__
+
+    map_name, reduce_name = map(lambda x: x.capitalize(), [map_name, reduce_name])
+
+    return type(
+        f'Slice{map_name}{reduce_name}', (SliceMapReduce,), d
+    )
 
 
 if __name__ == '__main__':
--- test/test_seq_variables.py
+++ test/test_seq_variables.py
@@ -1,6 +1,7 @@
 import pytest
+from toolz import curry
 
-from lXtractor.variables.sequential import SeqEl, PFP
+from lXtractor.variables.sequential import SeqEl, PFP, make_rmr
 from lXtractor.core.exceptions import FailedCalculation
 
 
@@ -37,3 +38,35 @@ def test_prot_fp(simple_chain_seq):
     with pytest.raises(FailedCalculation):
         s.add_seq('I', [1, 2, 3, 4, 5])
         PFP(6, 1).calculate(s['I'])
+
+
+def test_range_map_reduce_factory():
+    v = make_rmr(sum, float)()
+    assert v.id == "SliceSum(start=None,stop=None,step=None,seq_name='seq1')"
+    assert v.rtype is float
+
+    v = make_rmr(
+        sum, int, reduce_fn_name='Summer',
+        map_fn=lambda _: 'whatever', map_fn_name='whatever')
+    assert v.__name__ == 'SliceWhateverSummer'
+
+
+@curry
+def count_char(seq: str, c: str):
+    return sum(1 for x in seq if x == c)
+
+
+def test_range_map_reduce(simple_chain_seq):
+    _, s = simple_chain_seq
+    s.add_seq('X', [1, 2, 3, 2, 1])
+    v = make_rmr(sum, float)(start=1, stop=2)
+    assert v.calculate(s['X']) == 3
+    v = make_rmr(count_char(c='A'), int, reduce_fn_name='CounterA')()
+    assert v.calculate(s.seq1) == 1
+    v = make_rmr(
+        count_char(c='e'), int,
+        map_fn=lambda x: "".join(x).lower(),
+        reduce_fn_name='counter',
+        map_fn_name='lower'
+    )()
+    assert v.calculate(s.seq1) == 1
