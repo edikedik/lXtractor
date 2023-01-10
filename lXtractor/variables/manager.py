7c3032058749b559c29828327b78b9e28e6f8a73
Ivan Reveguk <edikedikedikedik@gmail.com>
2023-01-10 19:14:44 +0200

    `Manager` docs and doctests

--- lXtractor/variables/manager.py
+++ lXtractor/variables/manager.py
@@ -1,3 +1,15 @@
+"""
+``Manager`` handles variable calculations, such as:
+
+#. Variable manipulations (assignment, deletions, and resetting).
+#. Calculation of variables. Simply manages the calculation process, whereas
+    calculators (:class:`lXtractor.variables.calculator.GenericCalculator`
+    for instance) do the heavy lifting.
+#. Aggregation of the calculation results, either
+    :meth:`from_chains <Manager.aggregate_from_chains>` or
+    :meth:`from_iterable <Manager.aggregate_from_it>`.
+
+"""
 import logging
 import typing as t
 from collections import abc, defaultdict
@@ -6,7 +18,7 @@ from itertools import chain, groupby, repeat, tee
 import biotite.structure as bst
 import numpy as np
 import pandas as pd
-from more_itertools import unzip
+from more_itertools import unzip, peekable
 from toolz import curry
 from tqdm.auto import tqdm
 
@@ -46,9 +58,30 @@ def _update_variables(vs: Variables, upd: abc.Iterable[SoSv]) -> Variables:
     return vs
 
 
-def get_mapping(
-    obj: t.Any, map_name: str | None, map_to: str | None
-) -> t.Optional[dict[int, int]]:
+def get_mapping(obj: t.Any, map_name: str | None, map_to: str | None) -> dict | None:
+    """
+    Obtain mapping from a Chain*-type object.
+
+    >>> s = ChainSequence.from_string('ABCD', name='seq')
+    >>> s.add_seq('some_map', [5, 6, 7, 8])
+    >>> s.add_seq('another_map', ['D', 'B', 'C', 'A'])
+    >>> get_mapping(s, 'some_map', None)
+    {5: 1, 6: 2, 7: 3, 8: 4}
+    >>> get_mapping(s, 'another_map', 'some_map')
+    {'D': 5, 'B': 6, 'C': 7, 'A': 8}
+
+    :param obj: Chain*-type object. If not a Chain*-type object,
+        raises `AttributeError`.
+    :param map_name: The name of a map to create the mapping from.
+        If ``None``, the resulting mapping is ``None``.
+    :param map_to: The name of a map to create a mapping to.
+        If ``None``, will default to the real sequence indices (1-based) for a
+        :class:`ChainSequence <lXtractor.core.chain.ChainSequence>` object
+        and to the structure actual numbering for the
+        :class:`ChainStructure <lXtractor.core.chain.ChainStructure>`.
+    :return: A dictionary mapping from the `map_name` sequence to `map_to`
+        sequence.
+    """
     if map_name is None:
         return None
 
@@ -74,16 +107,18 @@ def get_mapping(
 
 
 @t.overload
-def get_vs(obj: ChainStructure, missing) -> list[StructureVariable]:
+def _get_vs(obj: ChainStructure, missing) -> list[StructureVariable]:
     ...
 
 
 @t.overload
-def get_vs(obj: ChainSequence, missing) -> list[SequenceVariable]:
+def _get_vs(obj: ChainSequence, missing) -> list[SequenceVariable]:
     ...
 
 
-def get_vs(obj: SoS, missing: bool) -> list[SequenceVariable] | list[StructureVariable]:
+def _get_vs(
+    obj: SoS, missing: bool
+) -> list[SequenceVariable] | list[StructureVariable]:
     if missing:
         return [v for v, r in obj.variables.items() if r is None]
     return list(obj.variables)
@@ -108,24 +143,53 @@ def stage(
     map_name: str | None = None,
     map_to: str | None = None,
 ) -> StagedStr | StagedSeq:
+    """
+    Stage object for calculation. If it's a chain sequence, will stage some
+    sequence/mapping within it. If it's a chain structure, will stage the
+    atom array.
+
+    :param obj: Sequence/structure to calculate variables on.
+    :param vs: A sequence of variables to calculate.
+    :param missing: If ``True``, calculate only those assigned variables that
+        are missing.
+    :param seq_name: If `obj` is the chain sequence, the sequence name is used
+        to obtain an actual sequence (``obj[seq_name]``).
+    :param map_name: The mapping name to obtain the mapping keys.
+        If ``None``, the resulting mapping will be ``None``.
+    :param map_to: The mapping name to obtain the mapping values.
+        See :func:`get_mapping` for details.
+    :return: A tuple with four elements:
+        1. Original object.
+        2. Staged sequence or atom array.
+        3. A sequence of sequence or structural variables.
+        4. An optional mapping.
+    """
     target: ChainStructure | abc.Sequence | None
 
-    seq_vs, str_vs = _split_variables(vs or get_vs(obj, missing))
+    seq_vs, str_vs = _split_variables(vs or _get_vs(obj, missing))
     mapping = get_mapping(obj, map_name, map_to)
 
     if isinstance(obj, ChainStructure):
+        # TODO: searching always gets the original `obj`'s structure
+        # since the chain structure must have an atom array.
+        # to fix this, should I allow empty atom array?
         target = find_structure(obj)
         if isinstance(target, bst.AtomArray):
             return obj, target, str_vs, mapping
         raise MissingData(f'Failed to find structure for calculation on {obj}')
-    elif isinstance(obj, ChainSequence):
+    if isinstance(obj, ChainSequence):
         target = obj[seq_name]
         return obj, target, seq_vs, mapping
-    else:
-        raise TypeError(f'Invalid object type {type(obj)}')
+    raise TypeError(f'Invalid object type {type(obj)}')
 
 
 def find_structure(s: ChainStructure) -> bst.AtomArray | None:
+    """
+    Recursively search for structure up the ancestral tree.
+
+    :param s: An arbitrary chain structure.
+    :return: The first non-empty atom array up the parent chain.
+    """
     structure = s.pdb.structure
     parent = s.parent
     while structure is None and parent is not None:
@@ -153,12 +217,28 @@ def _split_variables(
 
 
 class Manager:
+    """
+    Manager of variable calculations, handling assignment, aggregation, and,
+    of course, the calculations themselves.
+    """
+
     __slots__ = ('verbose',)
 
     def __init__(self, verbose: bool = False):
+        """
+        :param verbose: Display progress bar.
+        """
         self.verbose = verbose
 
     def assign(self, vs: abc.Sequence[SoSv], chains: abc.Iterable[SoS]):
+        """
+        Assign variables to chains sequences/structures.
+
+        :param vs: A sequence of variables.
+        :param chains: An iterable over chain sequences/structures.
+        :return: No return. Will store assigned variables within the
+            `variables` attribute.
+        """
 
         seq_vs, str_vs = _split_variables(vs)
         seqs, strs = _split_objects(chains)
@@ -174,6 +254,15 @@ class Manager:
             o.variables.update({v: None for v in _vs})
 
     def remove(self, chains: abc.Iterable[SoS], vs: abc.Sequence[SoSv] | None = None):
+        """
+        Remove variables from the `variables` container.
+
+        :param chains: An iterable over chain sequences/structures.
+        :param vs: A sequence of variables to remove. If not provided, will
+            remove all variables.
+        :return: No return.
+        """
+
         def _take_key(v):
             return vs is not None and v in vs or vs is None
 
@@ -186,6 +275,16 @@ class Manager:
                 c.variables.pop(k)
 
     def reset(self, chains: abc.Iterable[SoS], vs: abc.Sequence[SoSv] | None = None):
+        """
+        Similar to :meth:`remove`, but instead of deleting, resets variable
+        calculation results.
+
+        :param chains: An iterable over chain sequences/structures.
+        :param vs: A sequence of variables to reset. If not provided, will
+            reset all variables.
+        :return: No return.
+        """
+
         def _take_key(v):
             return vs is not None and v in vs or vs is None
 
@@ -198,15 +297,39 @@ class Manager:
                 c.variables[k] = None
 
     def aggregate_from_chains(self, chains: abc.Iterable[SoS]) -> pd.DataFrame:
-        def _get_vs(obj: SoS) -> pd.DataFrame:
+        """
+        Aggregate calculation results from the `variables` container of the
+        provided chains.
+
+        >>> from lXtractor.variables.sequential import SeqEl
+        >>> s = ChainSequence.from_string('abcd', name='seq')
+        >>> manager = Manager()
+        >>> manager.assign([SeqEl(1)], [s])
+        >>> df = manager.aggregate_from_chains([s])
+        >>> len(df) == 1
+        True
+        >>> list(df.columns)
+        ['VariableID', 'VariableResult', 'ObjectID', 'ObjectType']
+
+        :param chains: An iterable over chain sequences/structures.
+        :return: A dataframe with `ObjectID`, `ObjectType`, and calculation
+            results.
+        """
+
+        def get_vs(obj: SoS) -> pd.DataFrame:
             vs_df = obj.variables.as_df()
             vs_df['ObjectID'] = obj.id
             vs_df['ObjectType'] = obj.__class__.__name__
             return vs_df
 
         vs: abc.Iterable[pd.DataFrame] = filter(
-            lambda x: len(x) > 0, map(_get_vs, chains)
+            lambda x: len(x) > 0, map(get_vs, chains)
         )
+        vs = peekable(vs)
+        if vs.peek(None) is None:
+            return pd.DataFrame(
+                columns=['VariableID', 'VariableResult', 'ObjectID', 'ObjectType']
+            )
 
         if self.verbose:
             vs = tqdm(vs, desc='Aggregating variables')
@@ -220,6 +343,26 @@ class Manager:
         replace_errors: bool = True,
         replace_errors_with: t.Any = np.NaN,
     ) -> pd.DataFrame | dict[str, list]:
+        """
+        Aggregate calculation results directly from :meth:`calculate` output.
+
+        :param results: An iterable over calculation results.
+        :param vs_to_cols: If ``True``, will attempt to use the wide format for
+            the final results with variables are columns. Otherwise, will use
+            the long format with fixed columns: "ObjectID", "VariableID",
+            "VariableCalculated", and "VariableResult". Note that for the wide
+            format to work, all objects variables were calculated on must have
+            unique IDs.
+        :param replace_errors: When calculation failed, replace the calculation
+            results with certain value.
+        :param replace_errors_with: Use this value to replace erroneous
+            calculation results.
+        :return: A table with results in long or short format. If the
+            conversion to the DataFrame fails, will output a default dictionary
+            holding aggregated results. It's a good idea to inspect this
+            dict to find values (lists) with unexpected lengths diverging from
+            the rest.
+        """
 
         d: t.DefaultDict[str, list] = defaultdict(list)
 
@@ -251,33 +394,44 @@ class Manager:
         try:
             return pd.DataFrame(d)
         except ValueError as e:
-            LOGGER.error(f'Failed to convert to a DataFrame (stacktrace below)')
+            LOGGER.error('Failed to convert to a DataFrame (stacktrace below)')
             LOGGER.exception(e)
             return d
 
     def stage(
-        self,
-        chains: abc.Iterable[SoS],
-        vs: abc.Sequence[SoSv] | None,
-        *,
-        missing: bool = True,
-        seq_name: str = SeqNames.seq1,
-        map_name: str | None = None,
-        map_to: str | None = None,
+        self, chains: abc.Iterable[SoS], vs: abc.Sequence[SoSv] | None, **kwargs
     ) -> abc.Generator[StagedSeq | StagedStr, None, None]:
+        """
+        Stage objects for calculations (e.g., using :meth:`calculate`).
+        It's a useful method if using a different calculation method and/or
+        parallelization strategy within a `Calculator` class.
 
-        _stage = curry(stage)(
-            vs=vs,
-            missing=missing,
-            seq_name=seq_name,
-            map_name=map_name,
-            map_to=map_to,
-        )
+        .. seealso::
+            :func:`stage`
+            :meth:`calculate`
+
+        >>> from lXtractor.core.chain import ChainSequence
+        >>> from lXtractor.variables.sequential import SeqEl
+        >>> s = ChainSequence.from_string('ABCD', name='seq')
+        >>> m = Manager()
+        >>> staged = list(m.stage([s], [SeqEl(1)]))
+        >>> len(staged) == 1
+        True
+        >>> staged[0]
+        (seq|1-4, 'ABCD', [SeqEl(p=1,_rtype='str',seq_name='seq1')], None)
+
+        :param chains: An iterable over chain sequences/structures.
+        :param vs: A sequence of variables. If not provided, will use assigned
+            variables (see :meth:`assign`).
+        :param kwargs: Passed to :func:`stage`.
+        :return: An iterable over tuples holding data for variables'
+            calculation.
+        """
 
         if self.verbose:
             chains = tqdm(chains, desc='Staging calculations')
 
-        yield from map(_stage, chains)
+        yield from map(curry(stage)(vs=vs, **kwargs), chains)
 
     def calculate(
         self,
@@ -288,6 +442,42 @@ class Manager:
         save: bool = False,
         **kwargs,
     ) -> abc.Generator[CalcRes, None, None]:
+        """
+        Handles variable calculations:
+
+            1. Stage calculations (see :meth:`stage`).
+            2. Calculate variables using the provided calculator.
+            3. (Optional) save the calculation results to variables container.
+            4. Output (stream) calculation results.
+
+        Note that 3 and 4 are done lazily as calculation results from the
+        calculator become available.
+
+        >>> from lXtractor.core.chain import ChainSequence
+        >>> from lXtractor.variables.calculator import GenericCalculator
+        >>> from lXtractor.variables.sequential import SeqEl
+        >>> s = ChainSequence.from_string('ABCD', name='seq')
+        >>> m = Manager()
+        >>> c = GenericCalculator()
+        >>> list(m.calculate([s], [SeqEl(1)], c))
+        [(seq|1-4, SeqEl(p=1,_rtype='str',seq_name='seq1'), True, 'A')]
+        >>> list(m.calculate([s], [SeqEl(5)], c))[0][-2:]
+        (False, 'Missing index 4 in sequence')
+
+        :param chains: An iterable over chain sequences/structures.
+        :param vs: A sequence of variables. If not provided, will use assigned
+            variables (see :meth:`assign`).
+        :param calculator: A calculator object -- some callable with the right
+            signature handling the calculations.
+        :param save: Save calculation results to variables. Will overwrite any
+            existing matching variables.
+        :param kwargs: Passed to :meth:`stage`.
+        :return: A generator over tuples:
+            1. Original object.
+            2. Variable.
+            3. Flag indicated whether the calculation was successful.
+            4. The calculation result (or the error message).
+        """
 
         objs, targets, variables, mappings = unzip(self.stage(chains, vs, **kwargs))
         variables1, variables2 = tee(variables)
