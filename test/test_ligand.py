e3d981fcd8b870a6e47a0d19e0705d415fde7f31
edikedik <edikedikedikedik@gmail.com>
2023-03-29 17:26:18 +0200

    improve ligand detection

--- test/test_ligand.py
+++ test/test_ligand.py
@@ -16,6 +16,7 @@ DATA = Path(__file__).parent / 'data'
         (DATA / '2oiq.cif', {('STI', ('A',))}),
         (DATA / '5hu9.cif', {('66K', ('A',))}),
         (DATA / '1aki.pdb', set()),
+        (DATA / '3uec.cif', set()),
         (
             DATA / '7fsh.cif',
             {
@@ -25,11 +26,12 @@ DATA = Path(__file__).parent / 'data'
                 ('UXA', ('C', 'D')),
             },
         ),
+        (DATA / '3unk.cif', {('0BY', ('A',))}),
     ],
 )
 def test_find_ligands(inp_id, expected):
     s = GenericStructure.read(inp_id)
-    ligands = list(find_ligands(s))
+    ligands = s.ligands
     d = defaultdict(set)
     for lig in ligands:
         d[lig.res_name] |= lig.parent_contact_chains
