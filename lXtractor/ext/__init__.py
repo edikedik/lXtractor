ece4d7733996a834bb0e5791b1de7392d54c5ca2
edikedik <edikedikedikedik@gmail.com>
2023-07-10 14:14:45 +0300

    import Pfam

--- lXtractor/ext/__init__.py
+++ lXtractor/ext/__init__.py
@@ -1,5 +1,5 @@
 from .alphafold import AlphaFold
-from .hmm import PyHMMer
+from .hmm import PyHMMer, Pfam
 from .pdb_ import PDB, filter_by_method
 from .sifts import SIFTS
 from .uniprot import fetch_uniprot
