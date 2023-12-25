3b510332ce9e173a86d629b095ea238f0005c804
edikedik <edikedikedikedik@gmail.com>
2023-12-25 17:50:44 +0300

    specialize collection; add_chains

--- lXtractor/collection.py
+++ lXtractor/collection.py
@@ -2,22 +2,50 @@ import logging
 import sqlite3
 import typing as t
 from collections import abc
+from itertools import chain
 from os import PathLike
 from pathlib import Path
 
+import pandas as pd
+
+import lXtractor.chain as lxc
 from lXtractor.core.exceptions import FormatError
+from lXtractor.util.typing import is_sequence_of
 
 LOGGER = logging.getLogger(__name__)
+_CT = t.TypeVar("_CT", lxc.ChainSequence, lxc.ChainStructure, lxc.Chain)
+_CT_MAP = {lxc.ChainSequence: 1, lxc.ChainStructure: 2, lxc.Chain: 3}
 
 
-class Collection:
-    def __init__(self, loc: str | PathLike):
+def _verify_chain_types(objs: abc.Sequence[t.Any], ct: t.Type[_CT]) -> None:
+    if not is_sequence_of(objs, ct):
+        raise TypeError(f"All chains must have the same type {ct}")
+
+
+class Collection(t.Generic[_CT]):
+    def __init__(self, loc: str | PathLike = ":memory:"):
         self._loc = loc if loc == ":memory:" else Path(loc)
         self._db = self._connect()
         self._setup()
+        self._chains: tuple[_CT, ...] = ()
+
+    @property
+    def loaded(self) -> lxc.ChainList[_CT]:
+        return lxc.ChainList(self._chains)
 
     def _connect(self) -> sqlite3.Connection:
         return sqlite3.connect(self._loc)
+
+    def _close(self) -> None:
+        self._db.close()
+
+    def _column_names_for(self, table_name) -> list[str]:
+        res = self._execute(f"SELECT * FROM {table_name}")
+        return [x[0] for x in res.description]
+
+    def _num_rows_for(self, table_name: str) -> int:
+        res = self._execute(f"SELECT COUNT(*) from {table_name}")
+        return len(res.fetchall())
 
     def _execute(
         self,
@@ -40,17 +68,15 @@ class Collection:
             LOGGER.error(f"Failed to execute statement {statement}")
             raise
 
-    def _column_names_for(self, table_name) -> list[str]:
-        res = self._execute(f"SELECT * FROM {table_name}")
-        return [x[0] for x in res.description]
-
     def _insert(
         self,
         table_name: str,
         data: abc.Sequence[abc.Sequence[t.Any]],
         columns: abc.Sequence[str] = None,
-        omit_first_id: bool = False
+        omit_first_id: bool = False,
     ):
+        if len(data) == 0:
+            return
         data_sizes = set(map(len, data))
         if len(data_sizes) > 1:
             raise FormatError(
@@ -85,10 +111,10 @@ class Collection:
             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             chain_id TEXT NOT NULL UNIQUE,
             chain_type INTEGER NOT NULL,
+            level INTEGER NOT NULL,
             FOREIGN KEY(chain_type) REFERENCES chain_types (type_id)
         ); """
         make_parents = """ CREATE TABLE IF NOT EXISTS parents (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             chain_id_parent TEXT NOT NULL,
             chain_id_child TEXT NOT NULL,
             FOREIGN KEY(chain_id_parent) REFERENCES chains (id)
@@ -99,7 +125,6 @@ class Collection:
                 ON DELETE CASCADE 
         ); """
         make_variables = """ CREATE TABLE IF NOT EXISTS variables (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
             chain_id INTEGER NOT NULL,
             variable_name TEXT NOT NULL,
             variable_value TEXT,
@@ -111,6 +136,13 @@ class Collection:
             FOREIGN KEY(variable_type_id) REFERENCES var_types (var_type_id)
             FOREIGN KEY(variable_rtype_ID) REFERENCES var_rtypes (rtype_id)
         ); """
+        make_paths = """ CREATE TABLE IF NOT EXISTS paths (
+            chain_id INTEGER NOT NULL PRIMARY KEY,
+            chain_path TEXT NOT NULL,
+            FOREIGN KEY(chain_id) REFERENCES chains (id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE
+        ); """
 
         for statement in [
             make_chain_types,
@@ -119,6 +151,7 @@ class Collection:
             make_chains,
             make_parents,
             make_variables,
+            make_paths,
         ]:
             self._execute(statement)
 
@@ -142,7 +175,118 @@ class Collection:
             ),
         ]
         for table_name, data in inserts:
-            self._insert(table_name, data, omit_first_id=True)
+            if self._num_rows_for(table_name) == 1:
+                self._insert(table_name, data, omit_first_id=True)
+
+    def get_table(
+        self, name: str, as_df: bool = False, **kwargs
+    ) -> list[str] | pd.DataFrame | abc.Iterator[pd.DataFrame]:
+        statement = f"SELECT * FROM {name}"
+        if as_df:
+            return pd.read_sql(statement, self._db, **kwargs)
+        return self._execute(statement).fetchall()
+
+    def _verify_chain_types(self, chains: t.Any) -> None:
+        pass
+
+    def _filter_existing_chains(self, chains: abc.Iterable[_CT]) -> abc.Iterator[_CT]:
+        existing_ids = self.get_chain_ids()
+        return filter(lambda x: x.id in existing_ids, chains)
+
+    def _filter_absent_chains(self, chains: abc.Iterable[_CT]) -> abc.Iterator[_CT]:
+        existing_ids = self.get_chain_ids()
+        return filter(lambda x: x.id not in existing_ids, chains)
+
+    def _filter_existing_ids(self, ids: abc.Iterable[str]) -> abc.Iterator[str]:
+        existing_ids = self.get_chain_ids()
+        return filter(lambda x: x in existing_ids, ids)
+
+    def get_chain_ids(
+        self, level: int | None = None, chain_type: str | int | None = None
+    ) -> list[str]:
+        statement = "SELECT chain_id FROM chains"
+        if level is not None:
+            statement += f" WHERE level = {level}"
+        if chain_type is not None:
+            statement += f" AND chain_type = {chain_type}"
+        res = self._execute(statement)
+        return [x[0] for x in res.fetchall()]
+
+    def _add_chains_data(
+        self, chains: lxc.ChainList[_CT], chain_type: int, level: int
+    ) -> None:
+        data = [(c.id, chain_type, level) for c in chains]
+        self._insert("chains", data, omit_first_id=True)
+        for i, children in enumerate(chains.iter_children(), start=1):
+            if children:
+                self._add_chains_data(children, chain_type, i)
+
+    def _add_parents_data(self, chains: lxc.ChainList[_CT]) -> None:
+        data = [(child.parent.id, child.id) for child in chains.collapse_children()]
+        self._insert("parents", data)
+
+    def add_chains(self, chains: abc.Sequence[_CT], load: bool = False):
+        if not chains:
+            return
+        self._verify_chain_types(chains)
+        chain_type = _CT_MAP[chains[0].__class__]
+        chains = lxc.ChainList[_CT](self._filter_absent_chains(chains))
+        self._add_chains_data(chains, chain_type, 0)
+        self._add_parents_data(chains)
+        if load:
+            self._chains += tuple(chains)
+
+
+class SequenceCollection(Collection[lxc.ChainSequence]):
+    def _verify_chain_types(self, chains: abc.Sequence[t.Any]) -> None:
+        _verify_chain_types(chains, lxc.ChainSequence)
+
+
+class StructureCollection(Collection[lxc.ChainStructure]):
+    def _verify_chain_types(self, chains: abc.Sequence[t.Any]) -> None:
+        _verify_chain_types(chains, lxc.ChainStructure)
+
+
+class ChainCollection(Collection[lxc.Chain]):
+    def __init__(self, loc: str | PathLike = ":memory:"):
+        super().__init__(loc)
+        self._make_structures_table()
+
+    def _make_structures_table(self):
+        make_structures = """ CREATE TABLE IF NOT EXISTS structures(
+            chain_id TEXT NOT NULL,
+            structure_id TEXT NOT NULL,
+            FOREIGN KEY(chain_id) REFERENCES chains(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE
+            FOREIGN KEY(structure_id) REFERENCES chains(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE
+        ); """
+        self._execute(make_structures)
+
+    def _add_chains_data(
+        self, chains: lxc.ChainList[_CT], chain_type: int, level: int
+    ) -> None:
+        # Add chain IDs
+        data = [(c.id, chain_type, level) for c in chains]
+        self._insert("chains", data, omit_first_id=True)
+        # Add chain structure IDs
+        data = list(
+            chain.from_iterable(
+                ((s.id, 2, level) for s in c.structures) for c in chains
+            )
+        )
+        self._insert("chains", data, omit_first_id=True)
+        # Add chain--structure relationships
+        data = list(
+            chain.from_iterable(((c.id, s.id) for s in c.structures) for c in chains)
+        )
+        self._insert("structures", data)
+
+        for i, children in enumerate(chains.iter_children(), start=1):
+            if children:
+                self._add_chains_data(children, chain_type, i)
 
 
 if __name__ == "__main__":
--- test/test_collection.py
+++ test/test_collection.py
@@ -1,6 +1,15 @@
-from lXtractor.collection import Collection
+import tempfile
+from pathlib import Path
+
 import pytest
 
+import lXtractor.chain as lxc
+from lXtractor.collection import (
+    Collection,
+    SequenceCollection,
+    ChainCollection,
+    StructureCollection,
+)
 
 GET_TABLE_NAMES = """SELECT name FROM sqlite_master WHERE type='table';"""
 TABLE_NAMES = (
@@ -10,13 +19,108 @@ TABLE_NAMES = (
     "chains",
     "parents",
     "variables",
-    "sqlite_sequence"
+    "paths",
+    "sqlite_sequence",
+)
+COLLECTION_TYPES = (
+    Collection,
+    SequenceCollection,
+    StructureCollection,
+    ChainCollection,
 )
 
 
-@pytest.mark.parametrize("loc", [":memory:"])
-def test_setup(loc):
-    col = Collection(loc)
+def get_all_ids(chains: lxc.ChainList):
+    if not chains:
+        return []
+    if isinstance(chains[0], lxc.Chain):
+        return (
+            chains.ids
+            + chains.structures.ids
+            + chains.collapse_children().ids
+            + chains.collapse_children().structures.ids
+        )
+    return [c.id for c in chains] + [c.id for c in chains.collapse_children()]
+
+
+def iter_parent_child_ids(chains: lxc.ChainList):
+    for child in chains.collapse_children():
+        yield child.parent.id, child.id
+
+
+@pytest.fixture()
+def chain_structures(simple_chain_structure):
+    c = simple_chain_structure.spawn_child(1, 20)
+    c.spawn_child(5, 10)
+    return lxc.ChainList([c])
+
+
+@pytest.fixture()
+def chain_sequences(chain_structures):
+    c = chain_structures[0].seq
+    c.spawn_child(1, 20)
+    return lxc.ChainList([c])
+
+
+@pytest.fixture()
+def chains(chain_structures):
+    c = lxc.Chain(chain_structures[0].seq, structures=chain_structures)
+    return lxc.ChainList([c])
+
+
+@pytest.mark.parametrize("cls", COLLECTION_TYPES)
+@pytest.mark.parametrize("loc", [":memory:", "file"])
+def test_setup(cls, loc):
+    if loc == ":memory:":
+        col = cls(loc)
+        handle = None
+    else:
+        handle = tempfile.NamedTemporaryFile("w")
+        col = cls(Path(handle.name))
+    table_names = set(TABLE_NAMES)
+    if cls is ChainCollection:
+        table_names.add("structures")
     with col._db as cur:
         names = cur.execute(GET_TABLE_NAMES)
-        assert set(x[0] for x in names) == set(TABLE_NAMES)
+        assert set(x[0] for x in names) == table_names
+
+    if handle is not None:
+        handle.close()
+
+
+@pytest.mark.parametrize("cls", COLLECTION_TYPES)
+def test_add_chains(cls, chain_sequences, chain_structures, chains):
+    col = cls()
+
+    if cls is SequenceCollection or cls is Collection:
+        cs = chain_sequences
+        ct = 1
+    elif cls is StructureCollection:
+        cs = chain_structures
+        ct = 2
+    else:
+        cs = chains
+        ct = 3
+
+    col.add_chains(cs, load=True)
+    ids = get_all_ids(cs)
+    added_ids = col.get_chain_ids()
+
+    assert added_ids == ids
+
+    ids_level0 = col.get_chain_ids(level=0, chain_type=ct)
+    assert ids_level0 == [c.id for c in cs]
+
+    # Test parent information incorporated correctly
+    ids_exp = list(iter_parent_child_ids(cs))
+    df = col.get_table("parents", as_df=True)
+    ids = [(row.chain_id_parent, row.chain_id_child) for _, row in df.iterrows()]
+    assert sorted(ids) == sorted(ids_exp)
+
+
+def test_add_chains_structures(chains):
+    # Test if structures are correctly added in chain collection
+    col = ChainCollection()
+    col.add_chains(chains)
+    df = col.get_table("structures", as_df=True)
+    assert chains.structures.ids == df["structure_id"].tolist()
