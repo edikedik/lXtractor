6194e34d8b63749f18cada50f510baa304fb94f3
edikedik <edikedikedikedik@gmail.com>
2024-02-07 15:47:00 +0200

    make UniProt available

--- lXtractor/ext/__init__.py
+++ lXtractor/ext/__init__.py
@@ -4,4 +4,4 @@ from .hmm import PyHMMer, Pfam, iter_hmm
 from .panther import fetch_orthologs_info
 from .pdb_ import PDB, filter_by_method
 from .sifts import SIFTS
-from .uniprot import fetch_uniprot
+from .uniprot import fetch_uniprot, UniProt
