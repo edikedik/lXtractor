25e6a8f5cf8322b6c2e974d2eff5ffd0d86e0c17
edikedik <edikedikedikedik@gmail.com>
2024-02-27 15:10:44 +0200

    improve logging

--- lXtractor/collection/collection.py
+++ lXtractor/collection/collection.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import pickle
 import sqlite3
@@ -9,6 +8,7 @@ from os import PathLike
 from pathlib import Path, PosixPath
 
 import pandas as pd
+from loguru import logger
 from more_itertools import spy, unzip
 from toolz import curry
 
@@ -18,7 +18,6 @@ from lXtractor.util.typing import is_sequence_of
 from lXtractor.variables.base import SequenceVariable, StructureVariable, LigandVariable
 from lXtractor.variables.manager import CalcRes
 
-LOGGER = logging.getLogger(__name__)
 _T = t.TypeVar("_T")
 _CT = t.TypeVar("_CT", lxc.ChainSequence, lxc.ChainStructure, lxc.Chain)
 _CT_MAP = {lxc.ChainSequence: 1, lxc.ChainStructure: 2, lxc.Chain: 3}
@@ -66,6 +65,8 @@ class Collection(t.Generic[_CT]):
         self._setup()
         self._chains: lxc.ChainList[_CT] = lxc.ChainList([])
 
+        logger.info(f"Initialized collection in {loc}.")
+
     @property
     def loaded(self) -> lxc.ChainList[_CT]:
         """
@@ -105,7 +106,7 @@ class Collection(t.Generic[_CT]):
                 else:
                     return cur.execute(statement, params)
         except Exception:
-            LOGGER.error(f"Failed to execute statement {statement}")
+            logger.error(f"Failed to execute statement {statement}")
             raise
 
     @staticmethod
@@ -131,7 +132,7 @@ class Collection(t.Generic[_CT]):
     ) -> tuple[str | None, abc.Iterable[abc.Sequence[_T]]]:
         data_size, data = self._peek_data_size(data)
         if data_size == -1:
-            LOGGER.warning(f"Attempting to insert empty `data` for {table_name}")
+            logger.warning(f"Attempting to insert empty `data` for {table_name}")
             return None, data
         placeholders = _make_placeholders(data_size)
         if columns is None:
@@ -325,6 +326,7 @@ class Collection(t.Generic[_CT]):
     ):
         data = [(c.id, chain_type, level, None) for c in chains]
         self._insert("chains", data)
+        logger.debug(f"Inserted data for {len(data)} chains.")
 
     def _add_chains_data(self, chains: lxc.ChainList[_CT], chain_type: int) -> None:
         for i, _chains in enumerate((chains, *chains.iter_children()), start=0):
@@ -334,6 +336,7 @@ class Collection(t.Generic[_CT]):
     def _add_parents_data(self, chains: lxc.ChainList[_CT]) -> None:
         data = [(child.parent.id, child.id) for child in chains.collapse_children()]
         self._insert("parents", data)
+        logger.debug(f"Inserted data for {len(data)} parent-child pairs.")
 
     def _add_chain_objects(self, chains: lxc.ChainList[_CT]) -> None:
         all_chains = chains + chains.collapse_children()
@@ -345,6 +348,7 @@ class Collection(t.Generic[_CT]):
         self._execute(statement, data, many=True)
         for c in all_chains:
             c.children = id2children[c.id]
+        logger.debug(f"Added {len(chains)} serialized chain objects.")
 
     def add(self, chains: abc.Sequence[_CT], load: bool = False) -> None:
         """
@@ -359,11 +363,17 @@ class Collection(t.Generic[_CT]):
         self._verify_chain_types(chains)
         chain_type = _CT_MAP[chains[0].__class__]
         chains = lxc.ChainList[_CT](self._filter_absent_chains(chains))
+        logger.debug(f"Adding {len(chains)} of type {chain_type}.")
+
         self._add_chains_data(chains, chain_type)
         self._add_parents_data(chains)
         self._add_chain_objects(chains)
         if load:
             self._chains += chains
+            logger.debug(
+                f"Loaded {len(chains)} more chains. "
+                f"Total loaded: {len(self._chains)}."
+            )
 
     def _recover_structures(self, chains: lxc.ChainList[_CT]) -> None:
         # Valid only for MappingCollection
@@ -453,6 +463,7 @@ class Collection(t.Generic[_CT]):
             self._recover_structures(chains)
         if keep:
             self._chains += chains
+        logger.debug(f"Loaded {len(chains)} chains from the database.")
         return chains
 
     def unload(self, chains: abc.Sequence[_CT] | abc.Sequence[str]) -> None:
@@ -465,6 +476,7 @@ class Collection(t.Generic[_CT]):
             return
         ids = chains if isinstance(chains[0], str) else lxc.ChainList(chains).ids
         self._chains = self.loaded.filter(lambda x: x.id not in ids)
+        logger.debug(f"Unloaded {len(chains)} chains from the database.")
 
     def remove(
         self,
@@ -496,6 +508,7 @@ class Collection(t.Generic[_CT]):
         ids = [(x,) for x in ids]
         self._execute(f"DELETE FROM {table} WHERE {column}=?", ids, many=True)
         self.unload(targets)
+        logger.debug(f"Removed data for {len(ids)} chains.")
 
     def add_vs(
         self,
@@ -527,6 +540,7 @@ class Collection(t.Generic[_CT]):
         )
         data = list(data)
         self._insert("variables", data)
+        logger.debug(f"Inserted {len(data)} variables.")
 
     def _expand_structures(self, chain_path: Path) -> abc.Iterable[Path]:
         """The method is only relevant for :class:`MappingCollection`, where
@@ -656,16 +670,23 @@ class MappingCollection(Collection[lxc.Chain]):
     def _insert_chains_data(
         self, chains: lxc.ChainList[_CT], chain_type: int, level: int
     ) -> None:
-        data_chains = ((c.id, chain_type, level, None) for c in chains)
-        data_structures = chain.from_iterable(
-            ((s.id, 2, level, None) for s in c.structures) for c in chains
+        data_chains = [(c.id, chain_type, level, None) for c in chains]
+        data_structures = list(
+            chain.from_iterable(
+                ((s.id, 2, level, None) for s in c.structures) for c in chains
+            )
         )
-        self._insert("chains", list(chain(data_chains, data_structures)))
+        self._insert("chains", chain(data_chains, data_structures))
+        logger.debug(
+            f"Inserted data for {len(chains)} sequences "
+            f"and {len(data_structures)} structures."
+        )
         # Add chain--structure relationships
         data = list(
             chain.from_iterable(((c.id, s.id) for s in c.structures) for c in chains)
         )
         self._insert("structures", data)
+        logger.debug(f"Inserted {len(data)} seq-str links to the structures table.")
 
     def _add_chain_objects(self, chains: lxc.ChainList[lxc.Chain]) -> None:
         all_chains = chains + chains.collapse_children()
@@ -676,8 +697,9 @@ class MappingCollection(Collection[lxc.Chain]):
             c.children = lxc.ChainList([])
             c.structures = lxc.ChainList([])
         statement = "UPDATE chains SET data=? WHERE id=?"
-        data = ((c, c.id) for c in chain(all_chains, structures))
+        data = [(c, c.id) for c in chain(all_chains, structures)]
         self._execute(statement, data, many=True)
+        logger.debug(f"Inserted raw {len(data)} chains.")
         for c in all_chains:
             c.children = id2children[c.id]
             c.structures = id2structures[c.id]
