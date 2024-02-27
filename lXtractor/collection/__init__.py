36c160f313e30b0fdfc9c6e9464e9acc68a069f2
edikedik <edikedikedikedik@gmail.com>
2024-02-27 11:04:43 +0200

    top-level imports

--- lXtractor/collection/__init__.py
+++ lXtractor/collection/__init__.py
@@ -1,5 +1,18 @@
-from .support import BatchData, BatchesHistory, ConstructorConfig, Interfaces
-from .collection import (
-    SequenceCollection, StructureCollection, MappingCollection
+from .support import (
+    SeqItem,
+    StrItem,
+    MapItem,
+    SeqItemList,
+    StrItemList,
+    MapItemList,
+    BatchData,
+    BatchesHistory,
+    ConstructorConfig,
+    Interfaces,
 )
-from .constructor import (SeqCollectionConstructor, StrCollectionConstructor)
+from .collection import SequenceCollection, StructureCollection, MappingCollection
+from .constructor import (
+    SeqCollectionConstructor,
+    StrCollectionConstructor,
+    MapCollectionConstructor,
+)
